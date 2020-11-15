2bc810ee273ab637877c24a817649a8d2170a9df
anbya <anbyaalibia@gmail.com>
2020-11-15 23:09:25 +0700

    xxx

--- pages/cartTab.js
+++ pages/cartTab.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import axios from "axios";
 import { connect } from "react-redux";
 import moment from 'moment'
-import { StyleSheet,View,ScrollView} from 'react-native'
+import Constants from 'expo-constants'
+import { StyleSheet,View,ScrollView, TouchableOpacity, Dimensions, Image} from 'react-native'
 import {
   Container,
   Header,
@@ -27,58 +28,177 @@ import {
 import { material } from "react-native-typography";
 import { Col, Row, Grid } from "react-native-easy-grid";
 import dataDummy from "./dummyData";
-import Constants from 'expo-constants'
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 
-class cartTab extends Component {
+class keranjang extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-      dataDummy: dataDummy
-      };
+    super(props);
+    this.state = {
+    dataDummy: dataDummy,
+    selectedIndex:0,
+    whislsitParameter:false
+    };
   } 
+  setSelectedIndex = event => {
+    const viewSize = event.nativeEvent.layoutMeasurement.width;
+    const contentOffset = event.nativeEvent.contentOffset.x;
+    const selectedIndex = Math.floor(contentOffset / viewSize);
+    this.setState({
+      ...this.state,
+      selectedIndex:selectedIndex
+    })
+  }
   render() {
+    const lebar = Dimensions.get("window").width
+    const tinggi = Dimensions.get("window").height
+    const tinggi5 = Dimensions.get("window").height*5/100
+    const tinggi10 = Dimensions.get("window").height*10/100
+    const { selectedIndex } = this.state
     return (
       <Container style={{paddingTop: Constants.statusBarHeight}}>
         <Grid>
           <Row size={1}>
             <Col>
-              <View style={{backgroundColor:"#f5f5f5",flex: 1,justifyContent:'center', alignItems:'flex-start',padding:10,borderRadius:20}}>
-                <Text style={{color:"#000000",fontSize:20,fontWeight:"bold"}}>My Shopping Cart</Text>
+              <View style={{ flex: 1, justifyContent: "center",alignItems:"center",backgroundColor:"#ffffff" }}>
+                <Row>
+                  <Col size={1}>
+                    <View style={{ flex: 1, justifyContent: "center",alignItems:"center"}}>
+                      <View>
+                        <Button transparent onPress={() => this.props.navigation.goBack()}>
+                          <MaterialCommunityIcons name={"arrow-left"} size={25} color={"#000000"} />
+                        </Button>
+                      </View>
+                    </View>
+                  </Col>
+                  <Col size={9}>
+                    <View style={{ flex: 1, justifyContent: "center",alignItems:"flex-start"}}>
+                      <Text numberOfLines={1} style={{color:"#000000",fontSize:20,fontWeight:"bold"}}>Keranjang</Text>
+                    </View>
+                  </Col>
+                </Row>
               </View>
             </Col>
           </Row>
-          <Row size={9}>
+          <Row size={8}>
             <Col>
-              <ScrollView>
-                {this.state.dataDummy.length > 0 && this.state.dataDummy.map((dataDummy,index) =>
-                  <View style={{backgroundColor:"#FFFFFF"}} key={index}>
-                    <Grid>
-                      <Row style={listStyle.borderBottom}>
-                        <Col size={2}>
-                          <View style={{flex: 1,justifyContent:'center', alignItems:'center',padding:10,borderRadius:20}}>
-                            <Thumbnail square source={{ uri: `http://anbyafile.jaygeegroupapp.com/assets/img/detailbg.jpg` }} />
-                          </View>
-                        </Col>
-                        <Col size={8}>
-                          <View style={{flex: 1,justifyContent:'center', alignItems:'flex-start',padding:10,borderRadius:20}}>
-                            <Text style={material.body2}>{dataDummy.title}</Text>
-                            <Text note>{dataDummy.subTitle}</Text>
-                          </View>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col size={10}>
-                          <View style={{flex: 1,justifyContent:'center', alignItems:'center',padding:10,borderRadius:20}}>
-                            <Text note>REDEEM FROM</Text>
-                            <Text style={material.body2}>2020-11-01 until 2020-11-05</Text>
-                          </View>
-                        </Col>
-                      </Row>
-                    </Grid>
+              <ScrollView style={{paddingLeft:0,backgroundColor:"#f5f5f5"}}>
+                <TouchableOpacity>
+                  <View style={listStyle.content2}>
+                    <Row style={{paddingTop:10,paddingBottom:10}}>
+                      <Col size={1}>
+                        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                          <MaterialCommunityIcons name={"checkbox-blank-outline"} size={25} color={"#019cde"} />
+                        </View>
+                      </Col>
+                      <Col size={9}>
+                        <View style={{flex:1,justifyContent:"center",alignItems:"flex-start"}}>
+                          <Text style={{color:"#000000",fontSize:15,fontWeight:"bold",marginLeft:5}}>Pilih semua barang</Text>
+                        </View>
+                      </Col>
+                    </Row>
                   </View>
-                )}
+                </TouchableOpacity>
+                <View style={listStyle.content2}>
+                  <Row style={{paddingTop:10,paddingBottom:10}}>
+                    <Col>
+                      <TouchableOpacity>
+                        <View>
+                          <Row>
+                            <Col size={1}>
+                              <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                                <MaterialCommunityIcons name={"checkbox-blank-outline"} size={25} color={"#019cde"} />
+                              </View>
+                            </Col>
+                            <Col size={9}>
+                              <View style={{flex:1,justifyContent:"center",alignItems:"flex-start"}}>
+                                <Text style={{color:"#000000",fontSize:15,fontWeight:"bold",marginLeft:5}}>xxxxxxxxxxx</Text>
+                                <Text style={{color:"#000000",fontSize:15,marginLeft:5}}>xxxxxxxxxxx</Text>
+                              </View>
+                            </Col>
+                          </Row>
+                        </View>
+                      </TouchableOpacity>
+                    </Col>
+                  </Row>
+                  <Row style={{paddingTop:10,paddingBottom:10}}>
+                    <Col size={1}>
+                      <TouchableOpacity>
+                        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                          <MaterialCommunityIcons name={"checkbox-blank-outline"} size={25} color={"#019cde"} />
+                        </View>
+                      </TouchableOpacity>
+                    </Col>
+                    <Col size={9}>
+                      <TouchableOpacity>
+                        <Row>
+                          <Col size={2}>
+                            <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                              <Image
+                                source={{
+                                uri: `https://via.placeholder.com/75`
+                                }}
+                                style={{ height: 75, width: 75,marginLeft:5}}
+                              />
+                            </View>
+                          </Col>
+                          <Col size={8}>
+                            <View style={{flex:1,justifyContent:"center",alignItems:"flex-start",padding:10}}>
+                              <Text style={{color:"#000000",fontSize:15,fontWeight:"bold",marginLeft:5}}>xxxxxxxxxxx</Text>
+                              <Text style={{color:"#000000",fontSize:15,marginLeft:5}}>xxxxxxxxxxx</Text>
+                            </View>
+                          </Col>
+                        </Row>
+                      </TouchableOpacity>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <View style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center"}}>
+                        <TouchableOpacity>
+                          <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <MaterialCommunityIcons name={"heart"} size={35} color={"#019cde"} />
+                          </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                          <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <MaterialCommunityIcons name={"trash-can"} size={35} color={"#019cde"} />
+                          </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                          <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <MaterialCommunityIcons name={"minus-box"} size={35} color={"#019cde"} />
+                          </View>
+                        </TouchableOpacity>
+                        <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                          <Text style={{color:"#000000",fontSize:35}}>xxx</Text>
+                        </View>
+                        <TouchableOpacity>
+                          <View style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <MaterialCommunityIcons name={"plus-box"} size={35} color={"#019cde"} />
+                          </View>
+                        </TouchableOpacity>
+                      </View>
+                    </Col>
+                  </Row>
+                </View>
               </ScrollView>
+            </Col>
+          </Row>
+          <Row size={1}>
+            <Col>
+              <View style={{ flex: 1, justifyContent: "center",alignItems:"center",backgroundColor:"#f5f5f5"}}>
+                <TouchableOpacity style={listStyle.addCart} onPress={() => alert("it's work")}>
+                  <View style={{ flex: 1, justifyContent: "center",alignItems:"center",flexDirection:"row"}}>
+                    <FontAwesome5 name={"cart-plus"} size={25} color={"#ffffff"} />
+                    <Text numberOfLines={1} style={{color:"#ffffff",fontSize:25,fontWeight:"bold"}}> Keranjang</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
             </Col>
           </Row>
         </Grid>
@@ -103,7 +223,66 @@ const listStyle = StyleSheet.create({
   borderAll:{
     borderWidth:1,
     borderColor:"#e6eaed"
+  },
+  pad5:{
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+  circleDiv:{
+    position:"absolute",
+    bottom: 15,
+    height: 10,
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  whiteCircle:{
+    width: 6,
+    height: 6,
+    borderRadius: 3,
+    margin: 5,
+    backgroundColor: "#000000"
+  },
+  carouselTag1:{
+    position:"absolute",
+    bottom:5,
+    left:5,
+    padding:5,
+    backgroundColor:"rgba(255, 255, 255, 0.8)"
+  },
+  content1:{
+    backgroundColor:"#ffffff",
+    flex:1,
+    justifyContent:"center",
+    alignItems:"flex-start",
+    paddingLeft:10,
+    paddingTop:5,
+    paddingRight:10,
+    paddingBottom:5,
+    marginBottom:5
+  },
+  content2:{
+    backgroundColor:"#ffffff",
+    flex:1,
+    justifyContent:"center",
+    alignItems:"flex-start",
+    paddingLeft:10,
+    paddingTop:5,
+    paddingRight:10,
+    paddingBottom:5,
+    marginTop:5,
+    marginBottom:5
+  },
+  addCart:{
+    backgroundColor:"#019cde",
+    width:"80%",
+    height:"60%",
+    borderRadius:15
   }
 })
 
-export default cartTab;
+export default keranjang;
